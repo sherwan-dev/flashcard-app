23f34a358d836dfb0c11b13c3881e36cc1e07c7e
sherwan <sherwanuodcs@gmail.com>
2025-12-11 21:47:51 +0100

    renamed the project

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SimpleCard from "@/components/cards/simple-card/simple-card";
 export default function Home() {
   return ( 
       <main className="">
-          <SimpleCard/>
+          <SimpleCard message="Hallo world."/>
       </main> 
   );
 }
